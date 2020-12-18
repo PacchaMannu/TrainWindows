c2caec8414dd898405a92d3920869f298b4dc57a
PacchaMannu <devganesh007@gmail.com>
2020-12-18 12:47:33 +0530

    next

--- dbconnect_02.py
+++ dbconnect_02.py
@@ -4,29 +4,39 @@ import datetime as D
 import json
 
 def Time():
-    tim=D.datetime.now()                                              #Get Time
+    """get current time"""
+    tim=D.datetime.now()                                              
     print('Time: ',tim)
     return tim
 
 def Query():
-    con=mysql.connector.connect(user='devganesh',                     #Send Query
-                            password='stilldev',
-                            host=E.host,
-                            port=3306,
-                            database='devsample')
+    """running sql command"""
+    con=mysql.connector.connect(user='devganesh',                    
+        password='stilldev',
+        host=E.host,
+        port=3306,
+        database='devsample')
     curs=con.cursor(dictionary=True)
     curs.execute("SELECT * FROM EMP_ACTIVE_DATE")
     res=curs.fetchall()
     jres=json.dumps(res,default=str,indent=4)
-    print("res type:",type(res[1]))
-    print("json type:",type(jres))
-    print(jres)
-    #for i in res:
+    #ult=json.loads(jres)             "json to dict"
+    # for i in ult:                   
+    #     print(i)
+
+    # print("res type:",type(res[1])) "result type"
+    
+    # print("json type:",type(jres))  "json output type"
+    
+    print(jres)                     #"print json"
+    
+    # for i in res:                   "print result"
     #    print(i)
     con.close()    
     
 def Duration(tim1,tim):
-    print('Duration: ',tim1-tim)                                      #Find time difference
+    """duration of sql command"""
+    print('Duration: ',tim1-tim)                                      
 
 t=Time()
 Query()
