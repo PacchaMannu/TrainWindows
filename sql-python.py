698d9927b9d00d498206475f8890f237627d7cb4
PacchaMannu <devganesh007@gmail.com>
2020-12-20 18:39:56 +0530

    3 done

--- sql-python.py
+++ sql-python.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import environment as E
-import datetime as D
+from datetime import datetime
 
 con=mysql.connector.connect(user='devganesh',                     #Send Query
     password='stilldev',
@@ -10,7 +10,7 @@ con=mysql.connector.connect(user='devganesh',                     #Send Query
 curs=con.cursor()
     
 def addStu():
-    stuId=curs.lastrowid
+    stuId = input("Enter Student ID: ")
     stuName = input("Enter Student name: ")
     stuDept = input("Enter Student department: ")
     stuCGPA = input("Enter Student CGPA: ")
@@ -20,8 +20,48 @@ def addStu():
         'stuDept': stuDept,
         'stuCGPA': stuCGPA
     }
-    
+    add_student = ("INSERT INTO STUDENT "
+                    "VALUES (%(stuId)s, %(stuName)s, %(stuDept)s, %(stuCGPA)s)")
+    curs.execute(add_student, add_stu)
+    con.commit()
 
+def addEmp():
+    comId = input("Enter Company ID: ")
+    comName = input("Enter Company name: ")
+    comLocation = input("Enter Company location: ")
+    comDate = None
+    while True:
+        print("Enter Interview date:",end=' ')
+        dat = str(input())
+        try:
+            comDate = datetime.strptime(dat, "%Y-%m-%d")
+            break
+        except:
+            print("Incorrect format. Enter again")
+    add_com = {
+        'comId': comId,
+        'comName': comName,
+        'comLocation': comLocation,
+        'comDate': comDate
+    }
+    add_company = ("INSERT INTO COMPANY "
+                    "VALUES (%(comId)s, %(comName)s, %(comLocation)s, %(comDate)s)")
+    curs.execute(add_company, add_com)
+    con.commit()
 
+def addPlace():
+    stuId = input("Enter Student ID: ")
+    comId = input("Enter Company ID: ")
+    Pack = input("Enter Package: ")
+    add_place = {
+        'stuId': stuId,
+        'comId': comId,
+        'Pack': Pack
+    }
+    add_placement = ("INSERT INTO PLACEMENTS "
+                    "VALUES (%(stuId)s, %(comId)s, %(Pack)s)")
+    curs.execute(add_placement, add_place)
+    con.commit()
 
+addPlace()
 con.close()
